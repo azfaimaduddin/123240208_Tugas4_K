f823d03b2acefabb0ba8a7061e8b0c15e92d0b09
azfaimaduddin <azfaimaduddin@gmail.com>
2025-11-20 02:01:52 +0700

    Update update.data.php

--- logic/update.data.php
+++ logic/update.data.php
@@ -16,11 +16,8 @@ if (
     $nohp = $_POST['nomer_hp'];
     $email = $_POST['email'];
     
-    // Ambil ID User yang sedang login untuk validasi keamanan
     $id_user_login = $_SESSION['id_user'];
 
-    // Query Update dengan VALIDASI KEPEMILIKAN (AND id_user = ...)
-    // Ini mencegah user mengedit data milik orang lain meskipun dia tahu ID-nya
     $update = mysqli_query($koneksi, "
         UPDATE peserta SET 
             nama = '$nama',
@@ -29,20 +26,14 @@ if (
         WHERE id_peserta = $id_peserta AND id_user = $id_user_login
     ");
 
-    // PERBAIKAN UTAMA ADA DI SINI:
-    // Mengganti 'pendaftar.php' menjadi 'pendaftaran.php'
-    
     if(mysqli_affected_rows($koneksi) > 0){
-        // Berhasil update
         header("Location: ../pendaftaran.php?id_tim=$id_tim&status=sukses_update");
     } else {
-        // Tidak ada baris yang berubah (bisa jadi data sama atau bukan pemiliknya)
         header("Location: ../pendaftaran.php?id_tim=$id_tim&status=tidak_ada_perubahan");
     }
     exit;
 
 } else {
-    // jika data tidak lengkap
     header("Location: ../index.php?status=input_tidak_lengkap");
     exit;
 }
