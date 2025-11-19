7e2e702349a29fc08443257d774de76c85e7fa6c
azfaimaduddin <azfaimaduddin@gmail.com>
2025-11-20 02:00:54 +0700

    Refactor delete.data.php for improved readability

--- logic/delete.data.php
+++ logic/delete.data.php
@@ -8,14 +8,11 @@ if (isset($_POST['id_peserta']) && isset($_POST['id_tim'])) {
     $id_tim = $_POST['id_tim'];
     $id_user_login = $_SESSION['id_user'];
 
-    // Query hapus dengan validasi kepemilikan (AND id_user = ...)
     $delete = mysqli_query($koneksi, "DELETE FROM peserta WHERE id_peserta = $id_peserta AND id_user = $id_user_login");
 
     if(mysqli_affected_rows($koneksi) > 0){
-        // Berhasil hapus
         header("Location: ../pendaftaran.php?id_tim=$id_tim");
     } else {
-        // Gagal hapus (mungkin bukan pemiliknya)
         header("Location: ../pendaftaran.php?id_tim=$id_tim&error=akses_ditolak");
     }
     exit;
@@ -24,4 +21,5 @@ if (isset($_POST['id_peserta']) && isset($_POST['id_tim'])) {
     header("Location: ../index.php?status=hapus_gagal");
     exit;
 }
+
 ?>
